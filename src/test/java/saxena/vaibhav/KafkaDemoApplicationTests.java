a4842d2d72563c9be47ab13124856f12c58461f9
Vaibhav Saxena <reachvaibhavsaxena@gmail.com>
2023-05-13 14:09:31 +0530

    Initial Commit

---
+++ src/test/java/saxena/vaibhav/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package saxena.vaibhav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
